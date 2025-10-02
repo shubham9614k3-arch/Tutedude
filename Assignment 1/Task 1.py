8f569982ce3c4c54b0cc69c831dc2c3b677e0a0d
shubham9614k3-arch <shubham9614k3@gmail.com>
2025-10-03 00:13:44 +0530

    Add files via upload

---
+++ Assignment 1/Task 1.py
@@ -0,0 +1,13 @@
+#Task 1
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the Second numbe:"))
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",multi)
+print("Division: ",div)
+
---
+++ Assignment 1/Task 2.py
@@ -0,0 +1,5 @@
+#Task 2
+
+c=input("Enter the first name: ")
+d=input("Enter the last name: ")
+print("Hello, ",c,d,"Welcome to the python program")
